d11005e6d70c21f0523eeba42cd6435bd95efedd
adair <adairxiao@gmail.com>
2019-11-09 17:54:05 +0800

    submit QQB

---
+++ H5ANDH5API/QQB/js/myQQ.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		// autoScrolling:true,
+        // scrollHorizontally: true,
+        // // 显示项目符号
+        navigation:true,
+        // 循环滚动
+        loopBottom:true,
+	});
+
+	//methods
+	// $.fn.fullpage.setAllowScrolling(false);
+});
